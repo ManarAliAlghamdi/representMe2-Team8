8d98edf68b27b1ae2169c944e2b29287ad4f0a68
Manar Alghamdi <manar.a.gh@hotmail.com>
2024-09-30 15:10:02 +0300

    manar Commit

---
+++ representMe2 Team8/ManarAlghamdi.swift
@@ -0,0 +1,18 @@
+//
+//  ManarAlghamdi.swift
+//  representMe2 Team8
+//
+//  Created by Manar Alghmadi on 27/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ManarAlghamdi: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ManarAlghamdi()
+}
