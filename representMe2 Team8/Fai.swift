3a339a59cf25971a8cc8110cac39ec4661e55563
Faialharthi <faialialharthi.d@gmail.com>
2024-09-30 15:00:11 +0300

    first edit

---
+++ representMe2 Team8/Fai.swift
@@ -0,0 +1,18 @@
+//
+//  Fai.swift
+//  representMe2 Team8
+//
+//  Created by Fai Alharthi on 27/03/1446 AH.
+//
+
+import SwiftUI
+
+struct Fai: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Fai()
+}
