3e96b298585f74223e2d9e1f9eec46549e99979b
aljoharah <j70725672@gmail.com>
2024-09-30 15:36:18 +0300

    test

---
+++ representMe2 Team8/Aljoharah.swift
@@ -0,0 +1,18 @@
+//
+//  Aljoharah.swift
+//  representMe2 Team8
+//
+//  Created by الجوهرة بنت عبدالله on 27/03/1446 AH.
+//
+
+import SwiftUI
+
+struct Aljoharah: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Aljoharah()
+}
