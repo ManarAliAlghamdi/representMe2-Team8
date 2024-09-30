47e1fa138e6613db5bf80f4430bd664f00f222bb
Albatoul <ebatoul25@gmail.com>
2024-09-30 15:40:18 +0300

    Test

--- representMe2 Team8/Albatoul.swift
+++ representMe2 Team8/Albatoul.swift
@@ -9,7 +9,46 @@ import SwiftUI
 
 struct Albatoul: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("AlbatoulAvata")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 250, height: 250)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                .shadow(radius: 8)
+                .padding(.top, 40)
+            
+            Text("Albatoul Alshahrani")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundStyle(
+                                LinearGradient(colors: [Color.orange.opacity(0.2), Color.mint.opacity(0.5)], startPoint: .top, endPoint: .bottom))
+                            .multilineTextAlignment(.center)
+                            .padding(.top)
+                        
+            Group {
+                            Text("Web Delveloper")
+                                .font(.title2)
+                            
+                            Text("Student At PNU")
+                                .font(.title2)
+                            
+                            Text("MUSIC Lover")
+                                .font(.title2)
+                                .foregroundStyle(
+                                    LinearGradient(colors: [Color.orange.opacity(0.5), Color.pink.opacity(0.5)], startPoint: .top, endPoint: .bottom)
+                                )
+                            
+                            Text("TAE GiRl love to drink coffee to Refresh")
+                                .font(.title2)
+                        }
+        }
+        .fontWeight(.medium)
+        .padding(.top, 10)
+        .padding(.bottom, 20)
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+
     }
 }
 
