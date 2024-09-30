0c46ebfb06111877f2ddd36e67a2c238322c10e5
Manar Alghamdi <manar.a.gh@hotmail.com>
2024-09-30 14:50:59 +0300

    Initial Commit

---
+++ representMe2 Team8/representMe2_Team8App.swift
@@ -0,0 +1,17 @@
+//
+//  representMe2_Team8App.swift
+//  representMe2 Team8
+//
+//  Created by Manar Alghmadi on 27/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct representMe2_Team8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
