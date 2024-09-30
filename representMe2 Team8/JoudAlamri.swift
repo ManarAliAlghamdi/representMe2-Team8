bd370a36c434b4c869989f33748ce7403be1bc8f
joudy003 <joudy.j.t@gmail.com>
2024-09-30 14:59:34 +0300

    test

---
+++ representMe2 Team8/JoudAlamri.swift
@@ -0,0 +1,18 @@
+//
+//  JoudAlamri.swift
+//  representMe2 Team8
+//
+//  Created by Joud Abdullah AlAmri on 9/30/24.
+//
+
+import SwiftUI
+
+struct JoudAlamri: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JoudAlamri()
+}
